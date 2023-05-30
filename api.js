152d4b941800fa0f1b025db1a606051006ab28a5
Maxence <maxencehedouin@gmail.com>
2023-05-30 10:30:41 +0200

    ajout du cors, validation auth middleware

--- api.js
+++ api.js
@@ -1,8 +1,18 @@
+require('dotenv').config();
 const auth = require('./middleware/api/auth');
+const cors = require('cors');
+
+const { env: { FRONT_CLIENT } } = process;
+
+const corsOptions = {
+    credentials: true,
+    optionsSuccessStatus: 200,
+    origin: FRONT_CLIENT,
+}
 
 function init(app, models) {
-    app.use('/auth', require('./routes/auth')(models));
-    app.use('/users', /*auth ,*/require('./routes/user')(models));
+    app.use('/auth',cors(corsOptions), require('./routes/auth')(models));
+    app.use('/users',cors(corsOptions), auth ,require('./routes/user')(models));
 }
 
 module.exports = { init };
--- middleware/api/auth.js
+++ middleware/api/auth.js
@@ -1,27 +1,14 @@
 const jwt = require('jsonwebtoken');
 var express = require('express')
 
-module.exports = (req, res, next) => {
-  try {
-    const token = req.headers.authorization;
-    
-    const decodedToken = jwt.verify(token, process.env.SECRET_TOKEN);
-    const userId = decodedToken.id;
-
-    // console.log(token);
-    if (token == "" || token == null) {
-    //   throw 'Invalid Token';
-      res.status(403).json({
-        message: 'Invalid Token! ' + token
-      });
-    } else {
+module.exports = async (req, res, next) => {
+    const token = req.cookies.token
+    jwt.verify(token, process.env.SECRET_TOKEN, (err, decodedToken) => {
+      if (err) {
+        res.clearCookie("token");
+        return res.sendStatus(401);
+      }
       next();
-    }
-  } catch(err) {
-    const { message, name } = err;
-    res.status(401).json({
-        message: 'Invalid Token! ' + req.headers.authorization,
+    })
 
-    });
-  }
-};
+  };
