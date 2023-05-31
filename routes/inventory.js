573a0bfc4ec6c02a4f7fe6ba680224fb5b65c286
Boris-Delacour <boris.delacour@outlook.fr>
2023-05-31 15:23:17 +0200

    Modif route inventory

--- routes/inventory.js
+++ routes/inventory.js
@@ -5,9 +5,10 @@ module.exports = function(models) {
 
     const setup = require("../controllers/inventory");
 
-    const { getInventoryByCharacterId } = setup(models);
+    const { getInventoryByCharacterId, getItemsInInventory } = setup(models);
 
     app.get("/:CharacterId", getInventoryByCharacterId);
+    app.get("/ItemsList", getItemsInInventory);
 
     return app;
 }
