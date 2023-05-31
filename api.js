1c29fb045bfa49202f8974493ba5ba80b8af4a1d
Boris-Delacour <boris.delacour@outlook.fr>
2023-05-31 11:35:04 +0200

    Add routes inventory, item, purse

--- api.js
+++ api.js
@@ -16,6 +16,9 @@ function init(app, models) {
     app.use('/auth',cors(corsOptions), require('./routes/auth')(models));
     app.use('/users',cors(corsOptions), auth ,require('./routes/user')(models));
     app.use('/characters',cors(corsOptions), auth ,require('./routes/character')(models));
+    app.use('/inventory',cors(corsOptions), auth ,require('./routes/inventory')(models));
+    app.use('/item',cors(corsOptions), auth ,require('./routes/item')(models));
+    app.use('/purse',cors(corsOptions), auth ,require('./routes/purse')(models));
     app.use('/campaign',cors(corsOptions), auth ,require('./routes/campaign')(models));
 }
 
