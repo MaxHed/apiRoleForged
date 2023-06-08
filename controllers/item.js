e1c54d353b0f3266021636650e9983b56816ddcb
Boris-Delacour <boris.delacour@outlook.fr>
2023-06-08 10:08:07 +0200

    Ajout création et modification Item

--- controllers/item.js
+++ controllers/item.js
@@ -12,8 +12,55 @@ module.exports = function ({ Item }) {
         res.json(items)
     }
     
+    const create = async (req, res) => {
+        const item = new Item({
+            name: req.body.name,
+            description: req.body.description,
+            slot: req.body.slot,
+            value: req.body.value,
+            max_size_stack: req.body.max_size_stack,
+            for_quest: req.body.for_quest,
+            createdAt: new Date(),
+            updatedAt: new Date()
+        });
+
+        await item.save().then(() => {
+            res.status(201).json({
+                message: "Item créé !",
+            })
+        }).catch((error) => {
+            res.status(403).json({
+                error,
+            })
+        });
+    }
+
+    const edit = async (req, res) => {
+        const item = Item.findOne({ where: { id: req.id } })
+
+        item.name = req.body.name
+        item.description = req.body.description
+        item.slot = req.body.slot
+        item.value = req.body.value
+        item.max_size_stack = req.body.max_size_stack
+        item.for_quest = req.body.for_quest
+        item.updatedAt = new Date()
+
+        await item.save().then(() => {
+            res.status(201).json({
+                message: "Item modifiée !",
+            })
+        }).catch((error) => {
+            res.status(403).json({
+                error,
+            })
+        });
+    }
+
     return {
         getItem,
-        getAll
+        getAll,
+        create,
+        edit
     }
 }
--- routes/item.js
+++ routes/item.js
@@ -5,10 +5,12 @@ module.exports = function(models) {
 
     const setup = require("../controllers/item");
 
-    const { getItem, getAll } = setup(models);
+    const { getItem, getAll, create, edit } = setup(models);
 
     app.get("/:id", getItem);
     app.get("/list", getAll);
+    app.post("/", create);
+    app.put("/", edit);
 
     return app;
 }
